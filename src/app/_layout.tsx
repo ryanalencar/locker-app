58662ab652563fc49beec1d4584bd56fa72c1390
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2025-07-20 19:02:32 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/app/_layout.tsx
@@ -0,0 +1,54 @@
+import '../styles/global.css';
+
+import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '../lib/constants';
+import { useColorScheme } from '../lib/useColorScheme';
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  ErrorBoundary
+} from 'expo-router';
+
+export default function RootLayout() {
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      document.documentElement.classList.add('bg-background');
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
+}
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
---
+++ src/app/index.tsx
@@ -0,0 +1,76 @@
+import { Controller, useForm } from "react-hook-form";
+import { View } from "react-native";
+import { Button } from "../components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../components/ui/card";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+import { Text } from "../components/ui/text";
+
+export default function Index() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  const handleLogin = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <View className="flex min-h-full items-center justify-center bg-gray-50 p-4">
+      <Card className="w-full max-w-lg shadow-lg">
+        <CardHeader className="flex flex-col items-center gap-2">
+          <CardTitle className="text-3xl font-bold">Smart Locker</CardTitle>
+          <CardDescription className="text-lg">Sistema de Gestão de Armários Inteligentes</CardDescription>
+        </CardHeader>
+        <CardContent className="p-6">
+          <View className="flex flex-col gap-4">
+            <View>
+              <Label nativeID='username'>Username</Label>
+              <Controller
+                control={control}
+                name="username"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input
+                    placeholder="Username"
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                  />
+                )}
+              />
+            </View>
+            <View>
+              <Label nativeID='password'>Password</Label>
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input
+                    placeholder="Password"
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                    secureTextEntry
+                  />
+                )}
+              />
+            </View>
+
+            <Button onPress={handleLogin}>
+              <Text>
+                {isLoading ? "Autenticando..." : "Entrar"}
+              </Text>
+            </Button>
+          </View>
+        </CardContent>
+      </Card>
+    </View>
+  );
+}
